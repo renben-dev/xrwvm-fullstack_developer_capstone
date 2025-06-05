07fae4c2a23e5e21ce1c43933976f69444979853
renben-dev <bidone.renzo@gmail.com>
2025-06-05 14:06:04 -0400

    made docker image name more semantic in docker-compose.yml+completed routes in app.js[this is for database app]

--- server/database/app.js
+++ server/database/app.js
@@ -82,16 +82,34 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+    try {
+        const dealerships_objs = await Dealerships.find();
+        res.json(dealerships_objs);
+    } catch (error) {
+        res.status(500).json({error: 'Error fetching dealerships data'});
+    }
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+    try {
+        const documents = await Dealerships.find({state: req.params.state});
+        res.json(documents);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching dealership' });
+    }
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({id: req.params.id});
+    res.json(documents);
+} catch (error) {
+    res.status(500).json({ error: 'Error fetching dealership' });
+}
 });
 
 //Express route to insert review
