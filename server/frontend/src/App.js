87eaa41300e507ec6ebba128d8c0bacd289588ec
renben-dev <bidone.renzo@gmail.com>
2025-06-05 10:47:06 -0400

    finished registration page

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,13 @@
 import LoginPanel from "./components/Login/Login"
+import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPanel />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/login" element={<LoginPanel />} />
+            <Route path="/register" element={<RegisterPanel />} />
+        </Routes>
+    );
 }
 export default App;
