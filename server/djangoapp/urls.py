39f1867d975097e1b9704f3803b6af620d045c0c
renben-dev <bidone.renzo@gmail.com>
2025-06-12 08:42:35 -0400

    rollig updates-django views

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
         path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
         path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
         path(route='dealer/<int_dealer_id>', view=views.get_dealer_details, name='get_dealer_details'),
+        path(route='reviews/delaer/<int:dealer_id', view=views.get_dealer_reviews, name="get_dealer_reviews"),
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
