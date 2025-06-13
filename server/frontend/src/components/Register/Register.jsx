9ae5c99bfa781faa656f42dfcfa21baaf6d8f3a4
renben-dev <bidone.renzo@gmail.com>
2025-06-13 10:10:06 -0400

    corrected userName to username in filed name in req body

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -31,7 +31,7 @@ const Register = () => {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                "userName": userName,
+                "usermame": userName,
                 "password": password,
                 "firstName": firstName,
                 "lastName": lastName,
